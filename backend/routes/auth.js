92c97f1df7a0e8fdcbcdc92c0e33e5e41404171b
R1j <chaitanyaraj809@gmail.com>
2026-06-16 17:35:20 +0530

    Fixed some error and backend started working properly

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,69 +1,69 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
 const generateToken = (id) => {
-    return jwt.sign({id},process.env.JWT_SECRET,{
-        expiresIn:'7d',
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '7d',
     })
 }
-router.post("/register",async(req,res)=>{
-    const{name,email,password}=req.body
-    try{
-        const userExist = await User.findOne({email});
-        if(userExist){
+
+router.post("/register", async (req, res) => {
+    const { name, email, password } = req.body
+    try {
+        const userExist = await User.findOne({ email });
+        if (userExist) {
             return res.status(400).json({
-                message:"user already exists"
+                message: "user already exists"
             });
         }
-        const salt = await bcrypt.salt(10);
-        const hashedpassword = await bcrypt.hash(password,salt);
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
         const user = await User.create({
             name,
             email,
-            password:hashedpassword,
+            password: hashedPassword,
         })
-        res.status(200).json({
-            _id:user._id,
-            name:user.name,
-            email:user.email,
-            token:generateToken(user._id),
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id),
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: 'server error', error: error.message,
         })
     }
-    catch(error){
-        res.status(401).json({
-            message:'server error',error:error.message,
-        })
-    } 
 })
-router.post("/login", async(req,res)=>{
-    try{
-        const {email,password} = req.body;
-        const user = await user.findOne({email});
-        if(!user){
-            rea.status(400).json({
-                message:"User not exists",
+
+router.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({
+                message: "User does not exist",
             });
         }
-        const isMatch = await user.compare(password,user.password);
-        if(!isMatch){
-            res.status(400).json({
-                message:"invalid password or username",
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({
+                message: "Invalid password or username",
             });
         }
         res.status(200).json({
-            _id:user._id,
-            name:user.name,
-            email:user.email,
-            token:generateToken(user._id),
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id),
         })
-    }
-    catch(error){
-        res.status(401).json({
-            message:"server error",
-            error:error.message,
+    } catch (error) {
+        res.status(500).json({
+            message: "server error",
+            error: error.message,
         })
     }
 })
