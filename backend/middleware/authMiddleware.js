1be46fe0d42a371a9a133fcaa1ae2933e6d95b99
R1j <chaitanyaraj809@gmail.com>
2026-06-16 15:29:10 +0530

    added a logic to middleware which give us secure routs

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+const protect = (req,res,next)=>{
+    let token;
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+        try{
+            token  = req.header.authorization.split(' ')[1];
+            const decoded = jwt.verify(token,process.env.JWT_SECRET)
+            req.user = decoded 
+            next();
+        }
+        catch(error){
+            res.statu(401).json({
+                message:'Not authorised,Token Failed',
+            })
+        }
+    }
+}
